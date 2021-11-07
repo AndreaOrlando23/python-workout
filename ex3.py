51d35d068d56798c17ad2dac0aa87331f589e577
AndreaOrlando23 <64850221+AndreaOrlando23@users.noreply.github.com>
2021-11-07 18:10:50 +0100

    feat: implement solution for ex3

--- ex3.py
+++ ex3.py
@@ -1,2 +1,23 @@
 # Run timing
 
+def run_timing():
+    """
+    Ask the user repeatedly for numeric input.
+    Prints the average time and number of runs.
+    """
+
+    num_of_runs = 0
+    total_time = 0
+    while True:
+        one_run = input("Enter 10 Km run time: ")
+        if not one_run:
+            break
+        num_of_runs += 1
+        total_time += float(one_run)
+    
+    average_time = total_time / num_of_runs
+
+    print(f"Average of {average_time:.2f}, over {num_of_runs} runs.")
+
+
+run_timing()
