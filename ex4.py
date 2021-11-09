7b12bc856f8d9a09fa7ac09ce2b3f5b1fd736f0c
AndreaOrlando23 <64850221+AndreaOrlando23@users.noreply.github.com>
2021-11-09 20:59:40 +0100

    feat: implement solution ex4

---
+++ ex4.py
@@ -0,0 +1,11 @@
+# Hexadecimal output
+
+def hex_output():
+    decnum = 0
+    hexnum = input('Enter a hex number to convert: ')
+    for power, digit in enumerate(reversed(hexnum)):
+        decnum += int(digit, 16) * (16 ** power)
+    print(decnum)
+
+
+hex_output()
