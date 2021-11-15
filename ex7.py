5563a5e65761b1b24e5b6b8fcdfd191d25005416
AndreaOrlando23 <64850221+AndreaOrlando23@users.noreply.github.com>
2021-11-15 20:41:57 +0100

    feat: manage capitalize letter

--- ex7.py
+++ ex7.py
@@ -5,10 +5,12 @@ def ubbi_dubbi(word):
     for letter in word:
         if letter in 'aeiou':
             output.append(f'ub{letter}')
+        elif letter in 'AEIOU':
+            output.append(f'Ub{letter.lower()}')
         else:
             output.append(letter)
         
     return ''.join(output)
 
 
-print(ubbi_dubbi('python'))
+print(ubbi_dubbi('Abba'))
